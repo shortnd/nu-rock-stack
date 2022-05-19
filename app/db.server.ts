f63f6daf793d1d0730c348a994dc00712929ccf8
shortnd <ococncol@gmail.com>
2022-05-19 09:00:21 -0400

    fixed some issues with mongo

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import invariant from "tiny-invariant";
 
 let prisma: PrismaClient;
 
@@ -12,51 +11,15 @@ declare global {
 // create a new connection to the DB with every change either.
 // in production we'll have a single connection to the DB.
 if (process.env.NODE_ENV === "production") {
-  prisma = getClient();
+  prisma = new PrismaClient();
+  prisma.$connect();
 } else {
   if (!global.__db__) {
-    global.__db__ = getClient();
+    global.__db__ = new PrismaClient();
+    global.__db__.$connect();
   }
   prisma = global.__db__;
 }
 
-function getClient() {
-  const { DATABASE_URL } = process.env;
-  invariant(typeof DATABASE_URL === "string", "DATABASE_URL env var not set");
-
-  const databaseUrl = new URL(DATABASE_URL);
-
-  const isLocalHost = databaseUrl.hostname === "localhost";
-
-  const PRIMARY_REGION = isLocalHost ? null : process.env.PRIMARY_REGION;
-  const FLY_REGION = isLocalHost ? null : process.env.FLY_REGION;
-
-  const isReadReplicaRegion = !PRIMARY_REGION || PRIMARY_REGION === FLY_REGION;
-
-  if (!isLocalHost) {
-    databaseUrl.host = `${FLY_REGION}.${databaseUrl.host}`;
-    if (!isReadReplicaRegion) {
-      // 5433 is the read-replica port
-      databaseUrl.port = "5433";
-    }
-  }
-
-  console.log(`🔌 setting up prisma client to ${databaseUrl.host}`);
-  // NOTE: during development if you change anything in this function, remember
-  // that this only runs once per server restart and won't automatically be
-  // re-run per request like everything else is. So if you need to change
-  // something in this file, you'll need to manually restart the server.
-  const client = new PrismaClient({
-    datasources: {
-      db: {
-        url: databaseUrl.toString(),
-      },
-    },
-  });
-  // connect eagerly
-  client.$connect();
-
-  return client;
-}
 
 export { prisma };
